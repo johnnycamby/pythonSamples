d71f6abe4456d294805424cbf4564aa5148bf1dc
johnnycamby <johnnycamby@gmail.com>
2016-04-20 07:04:12 +0200

    2nd commit

--- app/hello.py
+++ app/hello.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     #return '<h1>Hey, {0}!<h1>'.format(name)
-    return render_template('user.html')
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
